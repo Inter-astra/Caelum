4eaafc4928202cc04b076074486b71f5fd86215b
Tearsw <mehmtkan81@gmail.com>
2021-02-28 20:02:56 +0300

    Revert "fix?"
    This reverts commit 3bcb7925688826b36f2fd8342e5c6532ac422abe.

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -37,7 +37,6 @@ def promote(update: Update, context: CallbackContext) -> str:
     message = update.effective_message
     chat = update.effective_chat
     user = update.effective_user
-    member = chat.get_member(user.id)
 
     promoter = chat.get_member(user.id)
 
