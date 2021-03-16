5fb912db9c03b2252f3986240a763bbacfb47e94
Tearsw <mehmtkan81@gmail.com>
2021-03-16 19:56:14 +0300

    disable chat count

--- tg_bot/modules/sql/users_sql.py
+++ tg_bot/modules/sql/users_sql.py
@@ -156,13 +156,13 @@ def get_all_users():
         SESSION.close()
 
 
-def get_user_num_chats(user_id):
-    try:
-        return (
-            SESSION.query(ChatMembers).filter(ChatMembers.user == int(user_id)).count()
-        )
-    finally:
-        SESSION.close()
+# def get_user_num_chats(user_id):
+#     try:
+#         return (
+#             SESSION.query(ChatMembers).filter(ChatMembers.user == int(user_id)).count()
+#         )
+#     finally:
+#         SESSION.close()
 
 
 def get_user_com_chats(user_id):
