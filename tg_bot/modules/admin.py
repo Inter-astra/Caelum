b89c12a3cf6adb1ccf704ed87af8bd1c3edf8bde
Tearsw <mehmtkan81@gmail.com>
2021-03-24 12:18:31 +0300

    removed promote stuff

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -73,21 +73,6 @@ def promote(update: Update, context: CallbackContext) -> str:
     bot_member = chat.get_member(bot.id)
 
     try:
-        if user_id in SUDO_USERS or promoter.status == "creator":
-            bot.promoteChatMember(
-            chat.id,
-            user_id,
-            can_change_info=bot_member.can_change_info,
-            can_post_messages=bot_member.can_post_messages,
-            can_edit_messages=bot_member.can_edit_messages,
-            can_delete_messages=bot_member.can_delete_messages,
-            can_invite_users=bot_member.can_invite_users,
-            can_promote_members=bot_member.can_promote_members,
-            can_restrict_members=bot_member.can_restrict_members,
-            can_pin_messages=bot_member.can_pin_messages,
-        )
-
-        else:
             bot.promoteChatMember(
             chat.id,
             user_id,
