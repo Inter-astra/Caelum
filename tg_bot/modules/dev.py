b5abd4cef4e916e3b18d63623005f8baa4d8cc8c
Tearsw <mehmtkan81@gmail.com>
2021-03-25 20:10:55 +0300

    Synced with Kigyo

--- tg_bot/modules/dev.py
+++ tg_bot/modules/dev.py
@@ -4,10 +4,15 @@ import sys
 import platform
 from time import sleep
 from threading import Thread
-from tg_bot import OWNER_ID, dispatcher, updater, kp
+from tg_bot import OWNER_ID, dispatcher, updater
 from tg_bot.modules.helper_funcs.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.ext import CallbackContext, Filters, CommandHandler
+
+import asyncio
+from statistics import mean
+from time import monotonic as time
+from telethon import events
 
 
 @dev_plus
@@ -58,6 +63,50 @@ def restart(update: Update, context: CallbackContext):
     elif plt == "Linux":
         os.execv("start.sh", sys.argv)
 
+class Store:
+    def __init__(self, func):
+        self.func = func
+        self.calls = []
+        self.time = time()
+        self.lock = asyncio.Lock()
+
+    def average(self):
+        return round(mean(self.calls), 2) if self.calls else 0
+
+    def __repr__(self):
+        return f"<Store func={self.func.__name__}, average={self.average()}>"
+
+    async def __call__(self, event):
+        async with self.lock:
+            if not self.calls:
+                self.calls = [0]
+            if time() - self.time > 1:
+                self.time = time()
+                self.calls.append(1)
+            else:
+                self.calls[-1] += 1
+        await self.func(event)
+
+async def nothing(event):
+    pass
+
+
+messages = Store(nothing)
+inline_queries = Store(nothing)
+callback_queries = Store(nothing)
+
+telethn.add_event_handler(messages, events.NewMessage())
+telethn.add_event_handler(inline_queries, events.InlineQuery())
+telethn.add_event_handler(callback_queries, events.CallbackQuery())
+
+
+@telethn.on(events.NewMessage(pattern=r"/getstats", from_users=OWNER_ID))
+async def getstats(event):
+    await event.reply(
+        f"__CAELUM EVENT STATISTICS__**\n**Average messages:** {messages.average()}/s\n**Average Callback Queries:** {callback_queries.average()}/s\n**Average Inline Queries:** {inline_queries.average()}/s", parse_mode='md'
+        )
+
+
 LEAVE_HANDLER = CommandHandler("leave", leave, filters=Filters.user(OWNER_ID))
 GITPULL_HANDLER = CommandHandler(["gitpull", "update"], gitpull, filters=Filters.user(OWNER_ID))
 RESTART_HANDLER = CommandHandler(["reboot", "restart"], restart, run_async=True)
@@ -66,5 +115,4 @@ dispatcher.add_handler(LEAVE_HANDLER)
 dispatcher.add_handler(GITPULL_HANDLER)
 dispatcher.add_handler(RESTART_HANDLER)
 
-__mod_name__ = "Dev"
 __handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER]
